e5bbb71c1616ed1811f6c6bb1ac9dff8cccbd89d
Higor Oliveira <higorpiresoliveira@gmail.com>
2020-08-05 10:57:50 -0300

    :hammer: Config Heroku app

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,13 @@ import routes from './routes';
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.get('/', (req, res) => {
+    res.json({ message: "Hello World!" })
+})
 
 app.listen(process.env.PORT || 3333, () => {
     console.log('Server is running!');
